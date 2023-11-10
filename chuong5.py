bfeccb48b2601655b4fa307ee20f507158d3fc6c
02-vietanh-17A1 <148172805+02-vietanh-17A1@users.noreply.github.com>
2023-11-10 16:35:20 +0700

    Add files via upload

---
+++ chuong5.py
@@ -0,0 +1,35 @@
+#1.1
+print("**     **      ******    **          **        ********")
+print("**     **      **        **          **        **    **")
+print("*********      ******    **          **        **    **")
+print("**     **      **        **          **        **    **")
+print("**     **      ******    ******      ******    ********")
+
+
+#1.2
+x=10
+y=5
+print("tổng",x+y)
+print("hiệu",x-y)
+print("tích",x*y)
+print("thương",x/y)
+
+
+#1.3
+soluong=5
+dongia=25000
+thanhtien=soluong*dongia
+print('tên hàng : sữa hộp vinamilk')
+print('số lượng',dongia)
+print('thành tiền',thanhtien)
+
+
+#1.4
+import math
+
+a=float(input("4"))
+b=float(imput("5"))
+c=float(imput("6"))
+p=(a+b+c)/2
+s=math,sqrt(p*(p-a)*(p-b)*(p-c))
+print('diện tích tam giác là:',s)
